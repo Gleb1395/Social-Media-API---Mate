9a769398c387dcf577edfbc9cfd771e3c7bb8156
Gleb1395 <gleb1395@gmail.com>
2025-04-04 17:39:54 +0300

    Added JWT token, also installed django rest framework

--- src/manage.py
+++ src/manage.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import subprocess
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    if sys.argv[1].lower() == "test":
+        print("NOTE: Running black formatter")
+        subprocess.run(["black", "--config", ".black.toml", "."])
+        subprocess.run(["isort", "."])
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +23,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
